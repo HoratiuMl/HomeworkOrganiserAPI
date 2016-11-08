ddcdd525543b4e57c2cd32ad297add23e4389160
Mlendea Horatiu <Mlendea.Horatiu@GMail.com>
2016-11-09 01:14:19 +0200

    Added MVC to the project

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace HomeworkOrganiser.API
 {
@@ -12,6 +14,24 @@ namespace HomeworkOrganiser.API
                 .Build();
                 
             host.Run();
+        }
+
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to add services to the container.
+        /// </summary>
+        /// <param name="services"></param>
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// </summary>
+        /// <param name="app"></param>
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
