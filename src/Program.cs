8516129364e1ef68ac271bcaa6ad46490c72009f
Mlendea Horatiu <Mlendea.Horatiu@GMail.com>
2016-11-09 01:05:37 +0200

    Created a new basic .NET Core API project

---
+++ src/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Hosting;
+
+namespace HomeworkOrganiser.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IWebHost host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Program>()
+                .Build();
+                
+            host.Run();
+        }
+    }
+}
