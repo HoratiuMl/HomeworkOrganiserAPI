1306f0e2d5f09f85092b9a7ddfdef6b8acc25db3
Mlendea Horatiu <Mlendea.Horatiu@GMail.com>
2016-11-09 02:56:38 +0200

    Implemented a proper Homework model

--- src/Models/Homework.cs
+++ src/Models/Homework.cs
@@ -1,8 +1,19 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace HomeworkOrganiser.API.Models
 {
-    public class Homework
+    public class Homework : EntityBase
     {
-        public string Id = "id0";
-        public string Content = "This is just a placeholder";
+        [StringLength(32, ErrorMessage = "The {0} must be between {1} and {2} characters long", MinimumLength = 3)]
+        public string Title { get; set; }
+        
+        [StringLength(256, ErrorMessage = "The {0} must be between {1} and {2} characters long", MinimumLength = 3)]
+        public string Description { get; set; }
+
+        public DateTime Deadline { get; set; }
+
+        [Range(0, 10)]
+        public int Grade { get; set; }
     }
 }
